d5a87811dbf3e8b0e721da64e8ae099298c5bcfd
Erham Mergen <erham.mergen@gmail.com>
2019-05-13 23:53:33 +0300

    Added angular support to application

--- src/main/java/com/mergen/IsListesiApplication.java
+++ src/main/java/com/mergen/IsListesiApplication.java
@@ -1,11 +1,61 @@
 package com.mergen;
 
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class IsListesiApplication {
+@RestController
+public class IsListesiApplication extends SpringBootServletInitializer {
+	
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+	
+	@RequestMapping("/resource")
+	@ResponseBody
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
+	
+	@Configuration
+	@Order(SecurityProperties.BASIC_AUTH_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+	protected void configure(HttpSecurity http) throws Exception {
+	      http
+	      	.httpBasic()
+	      .and()
+	      	.authorizeRequests()
+	      		.antMatchers("/index.html", "/", "/home", "/login").permitAll()
+	      		.anyRequest().authenticated();
+	    }
+	  }
 
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(IsListesiApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(IsListesiApplication.class, args);
 	}
