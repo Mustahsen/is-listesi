d36ae31ca1ec9863c19e9fcb15389de6bca99687
Erham Mergen <erham.mergen@gmail.com>
2019-05-20 02:42:30 +0300

    cors

--- src/main/java/com/mergen/IsListesiApplication.java
+++ src/main/java/com/mergen/IsListesiApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class IsListesiApplication extends SpringBootServletInitializer {
@@ -16,5 +18,14 @@ public class IsListesiApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(IsListesiApplication.class, args);
 	}
+	
+	public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
 }
